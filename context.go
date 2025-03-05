1489bcdb312e2e6d902637cde2e439131e02850d
Anthony DiFrancesco <acdifran@gmail.com>
2025-03-05 10:25:30 -0800

    add Use directive

--- context.go
+++ context.go
@@ -41,7 +41,6 @@ var ctxOptionsKey ctxOptions
 // to skip the cache entry on Query.
 //
 //	client.T.Query().All(entcache.Skip(ctx))
-//
 func Skip(ctx context.Context) context.Context {
 	c, ok := ctx.Value(ctxOptionsKey).(*ctxOptions)
 	if !ok {
@@ -51,11 +50,24 @@ func Skip(ctx context.Context) context.Context {
 	return ctx
 }
 
+// Use returns a new Context that tells the Driver
+// to use the cache on Query and will overwrite previous Skip and Evict.
+//
+//	client.T.Query().All(entcache.Use(ctx))
+func Use(ctx context.Context) context.Context {
+	c, ok := ctx.Value(ctxOptionsKey).(*ctxOptions)
+	if !ok {
+		return context.WithValue(ctx, ctxOptionsKey, &ctxOptions{skip: false, evict: false})
+	}
+	c.skip = false
+	c.evict = false
+	return ctx
+}
+
 // Evict returns a new Context that tells the Driver
 // to skip and invalidate the cache entry on Query.
 //
 //	client.T.Query().All(entcache.Evict(ctx))
-//
 func Evict(ctx context.Context) context.Context {
 	c, ok := ctx.Value(ctxOptionsKey).(*ctxOptions)
 	if !ok {
@@ -71,7 +83,6 @@ func Evict(ctx context.Context) context.Context {
 // more than 1 SQL query (e.g. eager loading).
 //
 //	client.T.Query().All(entcache.WithKey(ctx, "key"))
-//
 func WithKey(ctx context.Context, key Key) context.Context {
 	c, ok := ctx.Value(ctxOptionsKey).(*ctxOptions)
 	if !ok {
@@ -84,7 +95,6 @@ func WithKey(ctx context.Context, key Key) context.Context {
 // WithTTL returns a new Context that carries the TTL for the cache entry.
 //
 //	client.T.Query().All(entcache.WithTTL(ctx, time.Second))
-//
 func WithTTL(ctx context.Context, ttl time.Duration) context.Context {
 	c, ok := ctx.Value(ctxOptionsKey).(*ctxOptions)
 	if !ok {
